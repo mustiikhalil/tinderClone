a2db13ccc74ae18b10a4054589198aa16dd0d0b3
mustii <mustii@mmk.one>
2018-12-20 23:08:33 +0300

    UI for registration page is done

--- tinder/Controllers/RegistrationViewController.swift
+++ tinder/Controllers/RegistrationViewController.swift
@@ -11,13 +11,72 @@ class RegistrationViewController: UIViewController {
     
     fileprivate let selectProfileImage: UIButton = {
         let btn = UIButton(type: .system)
-        
+        btn.setTitle("Select Image", for: .normal)
+        btn.backgroundColor = .white
+        btn.setTitleColor(.black, for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        btn.heightAnchor.constraint(equalToConstant: 270).isActive = true
+        btn.layer.cornerRadius = 16
         return btn
     }()
     
+    fileprivate let fullNameTextField: RegistrationTextField = {
+        let tf = RegistrationTextField(padding: 16)
+        tf.backgroundColor = .white
+        tf.placeholder = "Enter FullName"
+        return tf
+    }()
+    
+    fileprivate let emailNameTextField: RegistrationTextField = {
+        let tf = RegistrationTextField(padding: 16)
+        tf.placeholder = "Enter Email"
+        tf.backgroundColor = .white
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+    
+    fileprivate let passwordNameTextField: RegistrationTextField = {
+        let tf = RegistrationTextField(padding: 16)
+        tf.placeholder = "Enter Password"
+        tf.backgroundColor = .white
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    fileprivate let signUpButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Register", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .heavy)
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = #colorLiteral(red: 0.8128934503, green: 0.1175386086, blue: 0.3345344663, alpha: 1)
+        btn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        btn.layer.cornerRadius = 25
+        return btn
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        addGradient()
+        let stackView = UIStackView(arrangedSubviews: [selectProfileImage,
+                                                       fullNameTextField,
+                                                       emailNameTextField,
+                                                       passwordNameTextField,
+                                                       signUpButton])
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        view.addSubview(stackView)
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 32, bottom: 0, right: 32))
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
+    
+    fileprivate func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        let topColor = #colorLiteral(red: 0.974457562, green: 0.3630923927, blue: 0.3745511174, alpha: 1)
+        let bottomColor = #colorLiteral(red: 0.8920144439, green: 0.1395124793, blue: 0.4601837993, alpha: 1)
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.locations = [0, 1]
+        view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = view.frame
     }
 }
---
+++ tinder/Views/RegistrationTextField.swift
@@ -0,0 +1,37 @@
+//
+//  RegistrationTextField.swift
+//  tinder
+//
+//  Created by Mustafa Khalil on 12/20/18.
+//
+
+import UIKit
+
+class RegistrationTextField: UITextField {
+    
+    fileprivate let padding: CGFloat
+    // Gives a padding when the person is typing in the textfield
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    // Gives a padding for the text in the textfield when there is no editing
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    // returns the size of the object
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: 50)
+    }
+    
+    init(padding padding: CGFloat) {
+        self.padding = padding
+        super.init(frame: .zero)
+        layer.cornerRadius = 25
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

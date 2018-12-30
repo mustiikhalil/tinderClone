209b569cfcab5d17b215ee6a47a92a83cb8e3e09
mustii <mustii@mmk.one>
2018-12-30 18:10:08 +0300

    Refactoring

--- tinder/Controllers/MainViewController.swift
+++ tinder/Controllers/MainViewController.swift
@@ -11,6 +11,8 @@ import JGProgressHUD
 
 class MainViewController: UIViewController {
     
+    var lastFetchedUser: User?
+    
     private let cardDeckView = UIView()
     private let topView = TopNavigationStackView()
     private let bottomStackView = HomeButtonControlsStackView()
@@ -20,8 +22,6 @@ class MainViewController: UIViewController {
         return p
     }()
     
-    var lastFetchedUser: User?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -29,30 +29,11 @@ class MainViewController: UIViewController {
         fetchUsersFromFireStore()
     }
     
-    @objc func handleSettings() {
-        let vc = RegistrationViewController()
-        present(vc, animated: true)
-    }
-    
-    //MARK:- FilePrivate
+}
 
-    fileprivate func setupLayout() {
-        let mainStackView = UIStackView(arrangedSubviews: [topView, cardDeckView, bottomStackView])
-        mainStackView.axis = .vertical
-        view.addSubview(mainStackView)
-        mainStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
-        mainStackView.isLayoutMarginsRelativeArrangement = true
-        mainStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
-        mainStackView.bringSubviewToFront(cardDeckView)
-        
-        // setup buttons
-        topView.settingIcon.addTarget(self, action: #selector(handleSettings), for: .touchUpInside)
-        bottomStackView.refreshButton.addTarget(self, action: #selector(handleRefresh), for: .touchUpInside)
-    }
-    
-    @objc fileprivate func handleRefresh() {
-        fetchUsersFromFireStore()
-    }
+// MARK:- Firebase
+
+extension MainViewController {
     
     fileprivate func fetchUsersFromFireStore() {
         
@@ -76,6 +57,41 @@ class MainViewController: UIViewController {
         }
     }
     
+}
+
+// MARK:- Controls extension
+
+extension MainViewController {
+    
+    @objc fileprivate func handleRefresh() {
+        fetchUsersFromFireStore()
+    }
+    
+    @objc func handleSettings() {
+        let vc = RegistrationViewController()
+        present(vc, animated: true)
+    }
+    
+}
+
+// MARK:- UI
+
+extension MainViewController {
+
+    fileprivate func setupLayout() {
+        let mainStackView = UIStackView(arrangedSubviews: [topView, cardDeckView, bottomStackView])
+        mainStackView.axis = .vertical
+        view.addSubview(mainStackView)
+        mainStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        mainStackView.isLayoutMarginsRelativeArrangement = true
+        mainStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        mainStackView.bringSubviewToFront(cardDeckView)
+        
+        // setup buttons
+        topView.settingIcon.addTarget(self, action: #selector(handleSettings), for: .touchUpInside)
+        bottomStackView.refreshButton.addTarget(self, action: #selector(handleRefresh), for: .touchUpInside)
+    }
+    
     fileprivate func setupCards(user: User) {
         let cardView = CardView(frame: .zero)
         cardView.cardViewModel = user.toCardViewModel()
@@ -83,4 +99,5 @@ class MainViewController: UIViewController {
         cardDeckView.sendSubviewToBack(cardView)
         cardView.fillSuperview()
     }
+    
 }

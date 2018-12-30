383b9eba0abd9c7e6fbc8a5dbbfc16af4ce328a8
mustii <mustii@mmk.one>
2018-12-30 18:05:29 +0300

    Added pagenation

--- tinder/Controllers/MainViewController.swift
+++ tinder/Controllers/MainViewController.swift
@@ -7,15 +7,20 @@
 
 import UIKit
 import Firebase
-
+import JGProgressHUD
 
 class MainViewController: UIViewController {
     
     private let cardDeckView = UIView()
     private let topView = TopNavigationStackView()
     private let bottomStackView = HomeButtonControlsStackView()
+    private let progressHUD: JGProgressHUD = {
+        let p = JGProgressHUD(style: .dark)
+        p.textLabel.text = "Fetching Users"
+        return p
+    }()
     
-    var cardViewModels: [CardViewModel] = []
+    var lastFetchedUser: User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,21 +45,24 @@ class MainViewController: UIViewController {
         mainStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
         mainStackView.bringSubviewToFront(cardDeckView)
         
+        // setup buttons
         topView.settingIcon.addTarget(self, action: #selector(handleSettings), for: .touchUpInside)
+        bottomStackView.refreshButton.addTarget(self, action: #selector(handleRefresh), for: .touchUpInside)
     }
     
-    fileprivate func setupCards() {
-        cardViewModels.forEach { (cardVM) in
-            let cardView = CardView(frame: .zero)
-            cardView.cardViewModel = cardVM
-            cardDeckView.addSubview(cardView)
-            cardView.fillSuperview()
-        }
+    @objc fileprivate func handleRefresh() {
+        fetchUsersFromFireStore()
     }
     
     fileprivate func fetchUsersFromFireStore() {
-        let query = Firestore.firestore().collection("Users")
+        
+        progressHUD.show(in: view)
+        
+        let query = Firestore.firestore().collection("Users").order(by: "uid").start(after: [lastFetchedUser?.uid ?? ""]).limit(to: 2)
+        // pagenation
+        
         query.getDocuments { (snapshot, err) in
+            self.progressHUD.dismiss()
             if let err = err {
                 print("failed to fetch users: ", err.localizedDescription)
                 return
@@ -62,9 +70,17 @@ class MainViewController: UIViewController {
             snapshot?.documents.forEach({ (documentSnap) in
                 let userDictonary = documentSnap.data()
                 let user = User(dictionary: userDictonary)
-                self.cardViewModels.append(user.toCardViewModel())
+                self.lastFetchedUser = user
+                self.setupCards(user: user)
             })
-            self.setupCards()
         }
+    }
+    
+    fileprivate func setupCards(user: User) {
+        let cardView = CardView(frame: .zero)
+        cardView.cardViewModel = user.toCardViewModel()
+        cardDeckView.addSubview(cardView)
+        cardDeckView.sendSubviewToBack(cardView)
+        cardView.fillSuperview()
     }
 }
--- tinder/Views/HomeButtonControlsStackView.swift
+++ tinder/Views/HomeButtonControlsStackView.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class HomeButtonControlsStackView: UIStackView {
     
+    static func createButton(img: UIImage) -> UIButton {
+        let b = UIButton(type: .system)
+        b.setImage(img.withRenderingMode(.alwaysOriginal), for: .normal)
+        b.imageView?.contentMode = .scaleAspectFill
+        return b
+    }
+    
+    let refreshButton = createButton(img: #imageLiteral(resourceName: "be077009-7a36-471e-a50f-11a4e597829f"))
+    let dislikeButton = createButton(img: #imageLiteral(resourceName: "54797469-b618-491f-88e9-2824221065a6"))
+    let superlikeButton = createButton(img: #imageLiteral(resourceName: "b20a6d9a-233e-46da-bf38-96fdb801b723"))
+    let likeButton = createButton(img: #imageLiteral(resourceName: "49eda04c-035b-475c-bf42-c30a344445f5"))
+    let specialButton = createButton(img: #imageLiteral(resourceName: "4d860822-2a26-42ed-91f0-9a971494ffdd"))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -16,13 +28,7 @@ class HomeButtonControlsStackView: UIStackView {
         
         distribution = .fillEqually
         heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        let bottomStack = [#imageLiteral(resourceName: "be077009-7a36-471e-a50f-11a4e597829f"), #imageLiteral(resourceName: "54797469-b618-491f-88e9-2824221065a6"), #imageLiteral(resourceName: "b20a6d9a-233e-46da-bf38-96fdb801b723"), #imageLiteral(resourceName: "49eda04c-035b-475c-bf42-c30a344445f5"), #imageLiteral(resourceName: "4d860822-2a26-42ed-91f0-9a971494ffdd")].map { (img) -> UIView in
-            let b = UIButton(type: .system)
-            b.setImage(img.withRenderingMode(.alwaysOriginal), for: .normal)
-            return b
-        }
-        bottomStack.forEach { (v) in
+        [refreshButton, dislikeButton, superlikeButton, likeButton, specialButton].forEach { (v) in
             addArrangedSubview(v)
         }
     }
